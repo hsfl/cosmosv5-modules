4c5a3c4d51484095c2d1e188dcdb2bcfe71cb237
Eric Pilger <pilger@hsfl.hawaii.edu>
2026-08-26 14:14:50 -1000

    Update cmake to use submodule-relative paths with COSMOS_SOURCE fallback

--- cmake/use_cosmos_from_source.cmake
+++ cmake/use_cosmos_from_source.cmake
@@ -1,9 +1,16 @@
 if(NOT DEFINED COSMOS_MODULES_INCLUDED)
 set(COSMOS_MODULES_INCLUDED TRUE)
 
-include(${COSMOS_SOURCE}/agent/cmake/use_cosmos_from_source.cmake)
+get_filename_component(COSMOS_SOURCE_MODULES "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
 
-set(COSMOS_SOURCE_MODULES ${COSMOS_SOURCE}/modules)
+if(EXISTS "${COSMOS_SOURCE_MODULES}/deps/agent/cmake/use_cosmos_from_source.cmake")
+    include(${COSMOS_SOURCE_MODULES}/deps/agent/cmake/use_cosmos_from_source.cmake)
+elseif(DEFINED COSMOS_SOURCE)
+    include(${COSMOS_SOURCE}/agent/cmake/use_cosmos_from_source.cmake)
+else()
+    message(FATAL_ERROR "Cannot find cosmosv5-agent. Clone with --recurse-submodules or set COSMOS_SOURCE.")
+endif()
+
 message("Using COSMOS modules from " ${COSMOS_SOURCE_MODULES})
 
 include_directories(${COSMOS_SOURCE_MODULES}/libraries)
